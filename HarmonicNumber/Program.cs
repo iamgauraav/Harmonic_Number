fb8a361e17a080450ffb00aa6fdef695547d1daf
iamgauraav <iamgauraav@gmail.com>
2022-04-20 09:01:28 +0530

    Added Harmonic number

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+
+            double sum = 0.0;
+
+            for ( double count = 1; count <= num; count++)
+            {
+                Console.WriteLine("1/{0} +",count);
+
+                sum = sum + 1 / count;
+            }
+            Console.WriteLine("the sum of seies {0} is {1} ",num,sum);
+        }
+    }
+}
